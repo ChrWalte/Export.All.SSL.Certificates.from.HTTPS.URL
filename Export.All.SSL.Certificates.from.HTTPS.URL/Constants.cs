2f771b2a232fc6f3ab56565b288ad23bbcb8cefc
null-desktop\chris <chrwalte@protonmail.com>
2022-11-17 22:13:07 -0700

    inital project creation

---
+++ Export.All.SSL.Certificates.from.HTTPS.URL/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace Export.All.SSL.Certificates.from.HTTPS.URL;
+
+internal class Constants
+{
+    public const string ProgramName = "Export.All.SSL.Certificates.from.HTTPS.URL";
+    public const string ExportedCertificatesFolderName = "Exported-HTTPS-SSL-Certificates";
+    public const string DefaultHttpsUrl = "https://duckduckgo.com/";
+
+    public const string HttpsProtocol = "https://";
+    public const string HttpProtocol = "http://";
+
+    public const string Slash = "/";
+    public const int One = 1;
+    public const int Two = 2;
+    public const int Seven = 7;
+    public const int Eight = 8;
+    public const int Ten = 10;
+
+    public const string CertificateFileHeader = "-----BEGIN CERTIFICATE-----";
+    public const string CertificateFileFooter = "-----END CERTIFICATE-----";
+
+    public const string SerilogOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message}{NewLine}{Exception}";
+}
---
+++ Export.All.SSL.Certificates.from.HTTPS.URL/HashHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Export.All.SSL.Certificates.from.HTTPS.URL;
+
+internal class HashHelper
+{
+    public static string Sha512Hash(string input)
+    {
+        using var sha512 = SHA512.Create();
+
+        var stringBuilder = new StringBuilder();
+        var inputBytes = Encoding.ASCII.GetBytes(input);
+        foreach (var b in sha512.ComputeHash(inputBytes))
+            stringBuilder.Append(b.ToString("x2"));
+
+        return stringBuilder.ToString()[..Constants.Ten];
+    }
+}
